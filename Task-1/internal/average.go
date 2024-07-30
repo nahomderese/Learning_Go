d421568dd864006b9ad52cb16c19b80d1b2245f9
Nahom-Derese <nahomderese@gmail.com>
2024-07-30 16:46:06 +0300

    🧪 test(test module):
    Implemented a test for each functions in the task

--- Task-1/internal/average.go
+++ Task-1/internal/average.go
@@ -1,6 +1,11 @@
 package internal
 
 func AverageCalculator(scores map[string]float64) float64 {
+
+	if len(scores) == 0 {
+		return 0
+	}
+
 	scoresArray := make([]float64, 0, len(scores))
 	for _, value := range scores {
 		scoresArray = append(scoresArray, value)
