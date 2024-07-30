742ffc5b5342a2617f57ad950bdde86b62a82cf3
Nahom-Derese <nahomderese@gmail.com>
2024-07-31 00:27:09 +0300

    feat: Refactor code to handle negative numbers and implement library management system

---
+++ Task-3/library_management/go.mod
@@ -0,0 +1,3 @@
+module github.com/Nahom-Derese/Learning_Go/Task-3/library_management
+
+go 1.22.5
---
+++ Task-3/library_management/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Nahom-Derese/Learning_Go/Task-3/library_management/controllers"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("Library Management System")
+		fmt.Println("1. Add a new book")
+		fmt.Println("2. Remove an existing book")
+		fmt.Println("3. Borrow a book")
+		fmt.Println("4. Return a book")
+		fmt.Println("5. List all available books")
+		fmt.Println("6. List all borrowed books by a member")
+		fmt.Println("7. Exit")
+		fmt.Print("Enter your choice: ")
+
+		choice, _ := reader.ReadString('\n')
+		choice = strings.TrimSpace(choice)
+
+		switch choice {
+		case "1":
+			controllers.AddBook(reader)
+		case "2":
+			controllers.RemoveBook(reader)
+		case "3":
+			controllers.BorrowBook(reader)
+		case "4":
+			controllers.ReturnBook(reader)
+		case "5":
+			controllers.ListAvailableBooks()
+		case "6":
+			controllers.ListBorrowedBooks(reader)
+		case "7":
+			fmt.Println("Exiting...")
+			return
+		default:
+			fmt.Println("Invalid choice, please try again.")
+		}
+	}
+}
