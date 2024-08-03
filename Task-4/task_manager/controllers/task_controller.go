025791df8006c3d20a177c3854d003d428531bb2
Nahom-Derese <nahomderese@gmail.com>
2024-08-03 12:11:05 +0300

    feat: Add task controller and handlers for CRUD operations

---
+++ Task-4/task_manager/controllers/task_controller.go
@@ -0,0 +1,80 @@
+package controllers
+
+import (
+	"github.com/Nahom-Derese/Learning_Go/Task-4/task_manager/data"
+	"github.com/Nahom-Derese/Learning_Go/Task-4/task_manager/models"
+	"github.com/gin-gonic/gin"
+)
+
+type TaskHandlers interface {
+	GetAllTasks() gin.HandlerFunc
+	GetTaskById() gin.HandlerFunc
+	UpdateTask() gin.HandlerFunc
+	DeleteTask() gin.HandlerFunc
+	CreateTask() gin.HandlerFunc
+}
+
+type TaskController struct {
+	TaskRepo data.TaskRepository
+}
+
+func (ctrl *TaskController) GetAllTasks() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tasks := ctrl.TaskRepo.FindAll()
+		c.JSON(200, tasks)
+	}
+}
+
+func (ctrl *TaskController) GetTaskById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		task, err := ctrl.TaskRepo.FindByID(id)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+		c.JSON(200, task)
+	}
+}
+
+func (ctrl *TaskController) UpdateTask() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		task, err := ctrl.TaskRepo.FindByID(id)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+		c.BindJSON(&task)
+		ctrl.TaskRepo.Save(task)
+		c.JSON(200, task)
+	}
+}
+
+func (ctrl *TaskController) DeleteTask() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		err := ctrl.TaskRepo.Delete(id)
+		if err != nil {
+			c.JSON(404, gin.H{"error": "Task not found"})
+			return
+		}
+		c.JSON(200, gin.H{"message": "Task deleted"})
+	}
+}
+
+func (ctrl *TaskController) CreateTask() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var task models.Task
+		err := c.BindJSON(&task)
+
+		if err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctrl.TaskRepo.Save(&task)
+		c.JSON(200, task)
+
+	}
+}
