71ba52f65a27cdb4fa93a84f7ce5526fe64a9fb3
Nahom-Derese <nahomderese@gmail.com>
2024-08-03 12:09:05 +0300

    feat: Implement in-memory task repository

---
+++ Task-4/task_manager/data/task_service.go
@@ -0,0 +1,66 @@
+package data
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/Nahom-Derese/Learning_Go/Task-4/task_manager/models"
+)
+
+type TaskRepository interface {
+	Save(task *models.Task)
+	FindByID(id string) (*models.Task, error)
+	FindAll() []models.Task
+	Delete(id string) error
+}
+
+type InMemoryTaskRepository struct {
+	idCounter int
+	tasks     map[string]models.Task
+}
+
+type MongoTaskRepository struct {
+}
+
+func NewInMemoryTaskRepository() *InMemoryTaskRepository {
+	return &InMemoryTaskRepository{
+		idCounter: 1,
+		tasks:     make(map[string]models.Task),
+	}
+}
+
+func (repo *InMemoryTaskRepository) Save(task *models.Task) {
+	task.ID = strconv.Itoa(repo.idCounter)
+	repo.idCounter += 1
+	repo.tasks[task.ID] = *task
+}
+
+func (repo *InMemoryTaskRepository) FindByID(id string) (*models.Task, error) {
+	value, ok := repo.tasks[id]
+	if !ok {
+		return nil, errors.New("task not found")
+	}
+	return &value, nil
+}
+
+func (repo *InMemoryTaskRepository) FindAll() []models.Task {
+	tasks := make([]models.Task, 0, len(repo.tasks))
+	for _, value := range repo.tasks {
+		tasks = append(tasks, value)
+	}
+	return tasks
+}
+
+func (repo *InMemoryTaskRepository) Delete(id string) error {
+	_, ok := repo.tasks[id]
+	if !ok {
+		return errors.New("task not found")
+	}
+	delete(repo.tasks, id)
+	return nil
+
+}
+
+type TaskUseCase struct {
+	TaskRepo *TaskRepository
+}
