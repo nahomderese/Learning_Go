492c1a396594baedb783abc05176ddb73a5d1feb
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-08-07 12:11:56 +0300

    feat: Add user authentication middleware (#55)

--- Task-6/task_manager/router/router.go
+++ Task-6/task_manager/router/router.go
@@ -1,29 +1,66 @@
 package router
 
 import (
+	"net/http"
+	"strings"
+
 	"github.com/Nahom-Derese/Learning_Go/Task-6/task_manager/controllers"
+	"github.com/Nahom-Derese/Learning_Go/Task-6/task_manager/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func TaskHandlers(r *gin.Engine, ctrl controllers.TaskHandlers) {
+func TaskHandlers(r *gin.RouterGroup, ctrl controllers.TaskHandlers) {
 
 	// Tasks Endpoints
-	r.GET("/tasks", ctrl.GetAllTasks())
-	r.GET("/tasks/:id", ctrl.GetTaskById())
-	r.PUT("/tasks/:id", ctrl.UpdateTask())
-	r.DELETE("/tasks/:id", ctrl.DeleteTask())
-	r.POST("/tasks", ctrl.CreateTask())
+	r.GET("", ctrl.GetAllTasks())
+	r.GET(":id", ctrl.GetTaskById())
+
+	// Admin Middleware
+	r.Use(middleware.AdminAuthMiddleware())
+
+	r.POST("", ctrl.CreateTask())
+	r.PUT(":id", ctrl.UpdateTask())
+	r.DELETE(":id", ctrl.DeleteTask())
 
 }
 
-func UserHandlers(r *gin.Engine, ctrl controllers.UserHandlers) {
+func UserHandlers(r *gin.RouterGroup, ctrl controllers.UserHandlers) {
 
-	// User Endpoints
-	r.GET("/user", ctrl.GetAllUsers())
-	r.GET("/user/:id", ctrl.GetUserByUsername())
-	r.PUT("/user/:id", ctrl.UpdateUser())
-	r.DELETE("/user/:id", ctrl.DeleteUser())
-	r.POST("/user", ctrl.CreateUser())
+	// Users Endpoints
+	r.PUT(":id", ctrl.UpdateUser())
+	r.GET(":id", ctrl.GetUserByUsername())
+	r.DELETE(":id", ctrl.DeleteUser())
+
+	// Admin Middleware
+	r.Use(middleware.AdminAuthMiddleware())
+
+	r.GET("", ctrl.GetAllUsers())
+	r.PUT("/promote/:id", ctrl.PromoteUser())
+	r.POST("/promote/:id", ctrl.PromoteUser())
+	r.GET("/promote/:id", ctrl.PromoteUser())
+	r.DELETE("", ctrl.DeleteAllUsers())
+
+}
+
+func AuthHandlers(r *gin.RouterGroup, ctrl controllers.UserHandlers) {
+
+	// Auth Endpoints
+	r.POST("/register", ctrl.SignUp())
 	r.POST("/login", ctrl.Login())
 
+}
+
+func NoRouteHandler(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		// Check if the route starts with "/example"
+		if strings.HasPrefix(c.Request.URL.Path, "/users") || strings.HasPrefix(c.Request.URL.Path, "/tasks") || strings.HasPrefix(c.Request.URL.Path, "/auth") {
+			c.JSON(http.StatusMethodNotAllowed, gin.H{
+				"error": "Method not allowed",
+			})
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Route not found",
+			})
+		}
+	})
 }
