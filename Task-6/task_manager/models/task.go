1aec1e4556420a07dd40fb9fc4b7c1981530f6bb
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-08-07 12:11:56 +0300

    feat: Add user authentication middleware (#55)

--- Task-6/task_manager/models/task.go
+++ Task-6/task_manager/models/task.go
@@ -12,4 +12,5 @@ type Task struct {
 	Description string             `json:"description"`
 	DueDate     time.Time          `json:"due_date"`
 	Status      string             `json:"status"`
+	UserId      string             `json:"user_id"`
 }
