52026b7d469fa518cd988966976a6201a2ada458
Nahom Derese <65091771+Nahom-Derese@users.noreply.github.com>
2024-08-09 06:11:50 +0300

    refactor: Refactor user repository and usecases (#66)
    This commit refactors the user repository and usecases in the task_manager package. The changes improve the efficiency and consistency of the code related to user management. The user_repository.go file now includes methods for saving, updating, finding, and deleting users. The user_usecases.go file now includes methods for handling user-related use cases. These changes enhance the overall functionality and maintainability of the user management feature.

---
+++ Task-7/task_manager/Delivery/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/controllers"
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/data"
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/middleware"
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/router"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	gin.ForceConsoleColor()
+
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(gin.ErrorLogger())
+	// r.Use(contentType)
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	user_collection := client.Database("taskManager").Collection("users")
+	task_collection := client.Database("taskManager").Collection("tasks")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer disconnect(err, client)
+
+	taskRepo := data.NewMongoTaskRepository(task_collection)
+	userRepo := data.NewMongoUserRepository(user_collection)
+
+	taskHandler := &controllers.TaskController{TaskRepo: taskRepo, UserRepo: userRepo}
+	userHandler := &controllers.UserController{UserRepo: userRepo}
+	authHandler := &controllers.AuthController{UserRepo: userRepo}
+
+	taskRoutes := r.Group("tasks")
+	userRoutes := r.Group("users")
+	authRoutes := r.Group("auth")
+
+	// middlewares
+	taskRoutes.Use(middleware.AuthMiddleware(userRepo))
+	userRoutes.Use(middleware.AuthMiddleware(userRepo))
+
+	router.TaskHandlers(taskRoutes, taskHandler)
+	router.UserHandlers(userRoutes, userHandler)
+	router.AuthHandlers(authRoutes, authHandler)
+
+	r.Run("localhost:8000")
+}
+
+func disconnect(err error, client *mongo.Client) {
+	if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	}
+}
---
+++ Task-7/task_manager/Delivery/router/router.go
@@ -0,0 +1,47 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/controllers"
+	"github.com/Nahom-Derese/Learning_Go/Task-7/task_manager/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func TaskHandlers(r *gin.RouterGroup, ctrl controllers.TaskHandlers) {
+
+	// Tasks Endpoints
+	r.GET("", ctrl.GetAllTasks())
+	r.GET(":id", ctrl.GetTaskById())
+
+	r.POST("", middleware.AdminAuthMiddleware(), ctrl.CreateTask())
+	r.PUT(":id", middleware.AdminAuthMiddleware(), ctrl.UpdateTask())
+	r.DELETE(":id", middleware.AdminAuthMiddleware(), ctrl.DeleteTask())
+
+}
+
+func UserHandlers(r *gin.RouterGroup, ctrl controllers.UserHandlers) {
+
+	// Users Endpoints
+	r.PUT(":username", ctrl.UpdateUser())
+	r.GET(":username", ctrl.GetUser())
+	r.DELETE(":username", ctrl.DeleteUser())
+
+	r.GET("", middleware.AdminAuthMiddleware(), ctrl.GetAllUsers())
+	r.PATCH("/promote/:username", middleware.AdminAuthMiddleware(), ctrl.PromoteUser())
+
+}
+
+func AuthHandlers(r *gin.RouterGroup, ctrl controllers.AuthHandlers) {
+
+	// Auth Endpoints
+	r.POST("/register", ctrl.SignUp())
+	r.POST("/login", ctrl.Login())
+
+}
+
+func NoRouteHandler(r *gin.Engine) {
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "The requested route was not found"})
+	})
+}
