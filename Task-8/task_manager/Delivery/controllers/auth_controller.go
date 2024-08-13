e9f2e28a6caaf17fc68d3fbe094b3a009cdaf9e1
Nahom-Derese <nahomderese@gmail.com>
2024-08-13 16:59:16 +0300

    refactor: Update SignUp method in AuthController to use SignUp method in UserUseCase

--- Task-8/task_manager/Delivery/controllers/auth_controller.go
+++ Task-8/task_manager/Delivery/controllers/auth_controller.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/Nahom-Derese/Learning_Go/Task-8/task-manager/domain"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandlers interface {
@@ -30,34 +29,10 @@ func (ctrl *AuthController) SignUp() gin.HandlerFunc {
 			return
 		}
 
-		// hash password
-		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		newUser, e := ctrl.UserUsecase.Signup(user.Username, user.Password)
 
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		}
-
-		// if no users in the db, create an admin user
-		users := ctrl.UserUsecase.FindAll()
-
-		role := "regular"
-		if len(users) == 0 {
-			role = "admin"
-		}
-
-		// check if user already exists
-		_, exists := ctrl.UserUsecase.FindByUsername(user.Username)
-
-		if exists {
-			c.JSON(http.StatusConflict, gin.H{"error": "user already exists"})
-			return
-		}
-
-		userData := domain.User{Username: user.Username, Password: string(hash), Role: role}
-		newUser, error := ctrl.UserUsecase.CreateUser(userData)
-
-		if error != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": error.Error()})
+		if e != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": e.Error()})
 			return
 		}
 
