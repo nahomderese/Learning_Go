e730fde78ded940126d9a2cb2e0c3d37bd63f6e5
Nahom-Derese <nahomderese@gmail.com>
2024-08-13 15:12:40 +0300

    refactor: Update FindAll method in TaskRepository to return tasks and error

--- Task-8/task_manager/usecase/task_usecases_test.go
+++ Task-8/task_manager/usecase/task_usecases_test.go
@@ -1,1 +1,102 @@
+package usecase
 
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Nahom-Derese/Learning_Go/Task-8/task-manager/domain"
+	"github.com/Nahom-Derese/Learning_Go/Task-8/task-manager/mocks"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TaskUsecaseSuite struct {
+	suite.Suite
+	mockRepo      *mocks.TaskRepository
+	mockUser      domain.User
+	mockTask      domain.Task
+	mockEmptyTask domain.Task
+	mockUserID    primitive.ObjectID
+}
+
+func (suite *TaskUsecaseSuite) SetupTest() {
+	suite.mockRepo = mocks.NewTaskRepository(suite.T())
+	suite.mockUser = domain.User{
+		ID:       primitive.NewObjectID(),
+		Username: "username",
+		Role:     "admin",
+		Password: "password",
+	}
+	suite.mockUserID = primitive.NewObjectID()
+	suite.mockTask = domain.Task{
+		ID:          primitive.NewObjectID(),
+		Title:       "title",
+		Description: "description",
+		DueDate:     time.Now(),
+		Status:      "status",
+		UserId:      suite.mockUserID.Hex(),
+	}
+
+	suite.mockEmptyTask = domain.Task{}
+}
+
+func (suite *TaskUsecaseSuite) TestSuccessCreate() {
+
+	suite.mockRepo.On("Save", suite.mockTask).Return(suite.mockTask, nil).Once()
+
+	tu := NewTaskUseCase(suite.mockRepo)
+
+	task, err := tu.Save(context.TODO(), suite.mockTask)
+
+	suite.NoError(err)
+
+	suite.Equal(suite.mockTask, task)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+
+}
+
+func (suite *TaskUsecaseSuite) TestSuccessGetAll() {
+	suite.mockRepo.On("GetAll").Return([]domain.Task{suite.mockTask}, nil).Once()
+
+	tu := NewTaskUseCase(suite.mockRepo)
+
+	tasks, err := tu.FindAll(context.Background(), suite.mockUser)
+
+	suite.NoError(err)
+
+	suite.Equal([]domain.Task{suite.mockTask}, tasks)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestSuccessGetByID() {
+	suite.mockRepo.On("GetByID", suite.mockTask.ID).Return(suite.mockTask, nil).Once()
+
+	tu := NewTaskUseCase(suite.mockRepo)
+
+	task, err := tu.FindByID(context.Background(), suite.mockTask.ID)
+
+	suite.NoError(err)
+
+	suite.Equal(suite.mockTask, task)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func (suite *TaskUsecaseSuite) TestSuccessDelete() {
+	suite.mockRepo.On("Delete", suite.mockTask.ID).Return(nil).Once()
+
+	tu := NewTaskUseCase(suite.mockRepo)
+
+	err := tu.Delete(context.Background(), suite.mockTask.ID)
+
+	suite.NoError(err)
+
+	suite.mockRepo.AssertExpectations(suite.T())
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(TaskUsecaseSuite))
+}
